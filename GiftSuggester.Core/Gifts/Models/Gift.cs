47df4534df53827bd340ec107b6f805665f1f721
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-22 23:49:55 +0300

    feat: realise Gift and GiftDbModel

---
+++ GiftSuggester.Core/Gifts/Models/Gift.cs
@@ -0,0 +1,10 @@
+﻿namespace GiftSuggester.Core.Gifts.Models;
+
+public class Gift
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public Guid GroupId { get; set; }
+    public Guid PresenterId { get; set; }
+    public Guid RecipientId { get; set; }
+}
---
+++ GiftSuggester.Data/Gifts/Models/GiftDbModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GiftSuggester.Data.Gifts.Models;
+
+public class GiftDbModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public Guid GroupId { get; set; }
+    public Guid PresenterId { get; set; }
+    public Guid RecipientId { get; set; }
+}
