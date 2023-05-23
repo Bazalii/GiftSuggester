f890fd7a44876d75508554af9ab050edf50d79ee
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-23 22:28:15 +0300

    feat: realise User and UserDbModel

---
+++ GiftSuggester.Data/Users/Models/UserDbModel.cs
@@ -0,0 +1,14 @@
+﻿using GiftSuggester.Data.Groups.Models;
+
+namespace GiftSuggester.Data.Users.Models;
+
+public class UserDbModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Login { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public DateOnly DateOfBirth { get; set; }
+    public virtual List<GroupDbModel> Groups { get; set; } = new();
+}
