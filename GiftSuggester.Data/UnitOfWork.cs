566a5b90dfb2f3d8cf1ec12563b4b9bc7c448b58
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-24 00:55:24 +0300

    feat: realise UnitOfWork

---
+++ GiftSuggester.Data/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using GiftSuggester.Core.CommonClasses;
+
+namespace GiftSuggester.Data;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly GiftSuggesterContext _context;
+    
+    public UnitOfWork(GiftSuggesterContext context)
+    {
+        _context = context;
+    }
+
+    public Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        return _context.SaveChangesAsync(cancellationToken);
+    }
+}
