f037bef8c55e97bdaa8fcf6b92f0a8c62031f187
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-24 20:12:40 +0300

    feat: realise creation models for groups, gifts and users

---
+++ GiftSuggester.Core/Gifts/Models/GiftCreationModel.cs
@@ -0,0 +1,9 @@
+﻿namespace GiftSuggester.Core.Gifts.Models;
+
+public class GiftCreationModel
+{
+    public string Name { get; set; } = string.Empty;
+    public Guid GroupId { get; set; }
+    public Guid PresenterId { get; set; }
+    public Guid RecipientId { get; set; }
+}
