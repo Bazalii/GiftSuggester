6f2cdeadfde6ad804a73dcc96465be3dbf3c8b27
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-26 22:03:35 +0300

    feat: realise UserService

---
+++ GiftSuggester.Core/Users/Services/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using GiftSuggester.Core.Users.Models;
+
+namespace GiftSuggester.Core.Users.Services;
+
+public interface IUserService
+{
+    Task AddAsync(UserCreationModel creationModel, CancellationToken cancellationToken);
+    Task<User> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+    Task UpdateAsync(User user, CancellationToken cancellationToken);
+    Task RemoveByIdAsync(Guid id, CancellationToken cancellationToken);
+}
