50f89447baf3f87a04c1ff1dc2c6600eb2edf5f2
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-29 23:19:42 +0300

    feat: realise GroupController and necessary DTOs

---
+++ GiftSuggester.Web/Groups/Models/GroupResponse.cs
@@ -0,0 +1,10 @@
+﻿using GiftSuggester.Web.Users.Models;
+
+namespace GiftSuggester.Web.Groups.Models;
+
+public class GroupResponse
+{
+    public Guid Id { get; set; }
+    public Guid OwnerId { get; set; }
+    public List<UserResponse> Members { get; set; } = new();
+}
