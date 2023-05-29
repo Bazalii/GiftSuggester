588a479f040e8841c5ec73af39bd6529f73655d6
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-30 00:00:50 +0300

    feat: realise UserController and necessary DTOs

---
+++ GiftSuggester.Web/Users/Models/UserCreationRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace GiftSuggester.Web.Users.Models;
+
+public class UserCreationRequest
+{
+    public string Name { get; set; }
+    public string Login { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+}
---
+++ GiftSuggester.Web/Users/Models/UserResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace GiftSuggester.Web.Users.Models;
+
+public class UserResponse
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Login { get; set; }
+    public string Email { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public List<Guid> GroupIds { get; set; }
+}
