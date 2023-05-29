472d817b8b639e3616efddddfbf018215d006b83
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-05-30 00:00:56 +0300

    feat: realise ServiceNotRegisteredException

---
+++ GiftSuggester.Core/Exceptions/ServiceNotRegisteredException.cs
@@ -0,0 +1,9 @@
+﻿namespace GiftSuggester.Core.Exceptions;
+
+public class ServiceNotRegisteredException : Exception
+{
+    public ServiceNotRegisteredException(string message)
+        : base(message)
+    {
+    }
+}
