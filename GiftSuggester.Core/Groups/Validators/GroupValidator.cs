6ef13e95c9480d24100a2f6c8bc4742ddc140fcd
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-02 23:34:45 +0300

    feat: realise GroupValidator

---
+++ GiftSuggester.Core/Groups/Validators/GroupValidator.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using FluentValidation;
+using GiftSuggester.Core.Users.Repositories;
+using Group = GiftSuggester.Core.Groups.Models.Group;
+
+namespace GiftSuggester.Core.Groups.Validators;
+
+public class GroupValidator : AbstractValidator<Group>
+{
+    public GroupValidator(IUserRepository userRepository)
+    {
+        RuleFor(x => x.Name)
+            .Length(3, 100)
+            .WithMessage("{PropertyName} length has to be from {MinLength} characters to {MaxLength} characters!")
+            .Matches(new Regex("^[а-яА-Яa-zA-Z0-9\\s]*$"))
+            .WithMessage("{PropertyName} has to consist only of English or Russian letters, numbers and spaces!");
+        RuleFor(x => x.OwnerId)
+            .MustAsync(async (ownerId, cancellationToken) =>
+                           await userRepository.ExistsWithIdAsync(ownerId, cancellationToken))
+            .WithMessage("Owner with this id: {PropertyValue} doesn't exist!");
+    }
+}
