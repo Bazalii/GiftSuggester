2e399ccb613d8d3271dd727e8502482fe8e1d757
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-03 20:40:12 +0300

    chore & feat: realise mappers of gift models
    - Add Mapperly dependency to Core and Web projects

---
+++ GiftSuggester.Web/Gifts/Mappers/GiftWebModelsMapper.cs
@@ -0,0 +1,13 @@
+﻿using GiftSuggester.Core.Gifts.Models;
+using GiftSuggester.Web.Gifts.Models;
+using Riok.Mapperly.Abstractions;
+
+namespace GiftSuggester.Web.Gifts.Mappers;
+
+[Mapper]
+public partial class GiftWebModelsMapper
+{
+    public partial GiftCreationModel MapCreationRequestToCreationModel(GiftCreationRequest creationRequest);
+    public partial Gift MapUpdateRequestToGift(GiftUpdateRequest updateRequest);
+    public partial GiftResponse MapGiftToResponse(Gift gift);
+}
