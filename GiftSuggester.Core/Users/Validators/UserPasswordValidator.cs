8eb49121092421d1bb7f589b7b657dd6d28af7a5
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-05 23:24:08 +0300

    feat: realise UserPasswordValidator

---
+++ GiftSuggester.Core/Users/Validators/UserPasswordValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using GiftSuggester.Core.Users.Models;
+
+namespace GiftSuggester.Core.Users.Validators;
+
+public class UserPasswordValidator : AbstractValidator<User>
+{
+    public UserPasswordValidator()
+    {
+        RuleFor(x => x.Password)
+            .Length(8, 100)
+            .WithMessage("{PropertyName} length has to be from {MinLength} characters to {MaxLength} characters!")
+            .Matches("[A-Z]+")
+            .WithMessage("{PropertyName} has to contain at least one uppercase English letter!")
+            .Matches("[a-z]+")
+            .WithMessage("{PropertyName} has to contain at least one lowercase English letter!")
+            .Matches("[0-9]+")
+            .WithMessage("{PropertyName} has to contain at least one number!")
+            .Matches("[!?*.#_^]+")
+            .WithMessage("{PropertyName} has to contain at least one of special symbols: \"!,?,*,.,#,_,^\".");
+    }
+}
