2f7d80a8ba54c585e2082ff677690baac18d9f1c
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-05 23:25:29 +0300

    feat: improve UserValidator and GroupValidator
    - Remove creation of Regex objects
    - Add including of UserPasswordValidator in UserValidator

--- GiftSuggester.Core/Groups/Validators/GroupValidator.cs
+++ GiftSuggester.Core/Groups/Validators/GroupValidator.cs
@@ -12,7 +12,7 @@ public class GroupValidator : AbstractValidator<Group>
         RuleFor(x => x.Name)
             .Length(3, 100)
             .WithMessage("{PropertyName} length has to be from {MinLength} characters to {MaxLength} characters!")
-            .Matches(new Regex("^[а-яА-Яa-zA-Z0-9\\s]*$"))
+            .Matches("^[а-яА-Яa-zA-Z0-9\\s]*$")
             .WithMessage("{PropertyName} has to consist only of English or Russian letters, numbers and spaces!");
         RuleFor(x => x.OwnerId)
             .MustAsync(async (ownerId, cancellationToken) =>
