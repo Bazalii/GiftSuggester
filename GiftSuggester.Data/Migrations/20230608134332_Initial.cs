72460c551ccab0eb53ed703d27378e769bca2058
Ivan Bazalii <bazalijivan2002@yandex.ru>
2023-06-08 23:51:11 +0300

    chore: update migration files

--- GiftSuggester.Data/Migrations/20230608134332_Initial.cs
+++ GiftSuggester.Data/Migrations/20230608134332_Initial.cs
@@ -18,25 +18,13 @@ namespace GiftSuggester.Data.Migrations
                     id = table.Column<Guid>(type: "uuid", nullable: false),
                     name = table.Column<string>(type: "text", nullable: false),
                     group_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    creator_id = table.Column<Guid>(type: "uuid", nullable: false),
                     presenter_id = table.Column<Guid>(type: "uuid", nullable: false),
                     recipient_id = table.Column<Guid>(type: "uuid", nullable: false)
                 },
                 constraints: table =>
                 {
                     table.PrimaryKey("pk_gifts", x => x.id);
-                });
-
-            migrationBuilder.CreateTable(
-                name: "groups",
-                columns: table => new
-                {
-                    id = table.Column<Guid>(type: "uuid", nullable: false),
-                    name = table.Column<string>(type: "text", nullable: false),
-                    owner_id = table.Column<Guid>(type: "uuid", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("pk_groups", x => x.id);
                 });
 
             migrationBuilder.CreateTable(
@@ -56,23 +44,66 @@ namespace GiftSuggester.Data.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "groups_users",
+                name: "groups",
                 columns: table => new
                 {
-                    groups_id = table.Column<Guid>(type: "uuid", nullable: false),
-                    members_id = table.Column<Guid>(type: "uuid", nullable: false)
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    owner_id = table.Column<Guid>(type: "uuid", nullable: false)
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("pk_groups_users", x => new { x.groups_id, x.members_id });
+                    table.PrimaryKey("pk_groups", x => x.id);
                     table.ForeignKey(
-                        name: "fk_groups_users_groups_groups_id",
-                        column: x => x.groups_id,
+                        name: "fk_groups_users_owner_id",
+                        column: x => x.owner_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "groups_admins",
+                columns: table => new
+                {
+                    admins_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    group_db_model_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_groups_admins", x => new { x.admins_id, x.group_db_model_id });
+                    table.ForeignKey(
+                        name: "fk_groups_admins_groups_group_db_model_id",
+                        column: x => x.group_db_model_id,
                         principalTable: "groups",
                         principalColumn: "id",
                         onDelete: ReferentialAction.Cascade);
                     table.ForeignKey(
-                        name: "fk_groups_users_users_members_id",
+                        name: "fk_groups_admins_users_admins_id",
+                        column: x => x.admins_id,
+                        principalTable: "users",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "groups_members",
+                columns: table => new
+                {
+                    group_db_model1id = table.Column<Guid>(type: "uuid", nullable: false),
+                    members_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_groups_members", x => new { x.group_db_model1id, x.members_id });
+                    table.ForeignKey(
+                        name: "fk_groups_members_groups_group_db_model1id",
+                        column: x => x.group_db_model1id,
+                        principalTable: "groups",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_groups_members_users_members_id",
                         column: x => x.members_id,
                         principalTable: "users",
                         principalColumn: "id",
@@ -80,8 +111,18 @@ namespace GiftSuggester.Data.Migrations
                 });
 
             migrationBuilder.CreateIndex(
-                name: "ix_groups_users_members_id",
-                table: "groups_users",
+                name: "ix_groups_owner_id",
+                table: "groups",
+                column: "owner_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_groups_admins_group_db_model_id",
+                table: "groups_admins",
+                column: "group_db_model_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_groups_members_members_id",
+                table: "groups_members",
                 column: "members_id");
 
             migrationBuilder.CreateIndex(
@@ -104,7 +145,10 @@ namespace GiftSuggester.Data.Migrations
                 name: "gifts");
 
             migrationBuilder.DropTable(
-                name: "groups_users");
+                name: "groups_admins");
+
+            migrationBuilder.DropTable(
+                name: "groups_members");
 
             migrationBuilder.DropTable(
                 name: "groups");
